7dd88a7d4f3b7be527911f6454a1dacba46a28b9
Chekaviah <chekaviah@gmail.com>
2017-12-05 11:35:06 +0100

    update urlExtractor

--- src/Service/UrlExtractor.php
+++ src/Service/UrlExtractor.php
@@ -8,39 +8,24 @@ class UrlExtractor
 	 * @param string $url
 	 * @return array
 	 */
-	public static function extractId(string $url): string
+	public static function extractId(string $url): array
 	{
-		$media = array(
-			'id' => '',
-			'website' => ''
-		);
-
 		//DAILYMOTION
-		preg_match('#&lt;object[^&gt;]+&gt;.+?http://www.dailymotion.com/swf/video/([A-Za-z0-9]+).+?&lt;/object&gt;#s', $url, $matches);
-		if(!isset($matches[1])) {
-			preg_match('#http://www.dailymotion.com/video/([A-Za-z0-9]+)#s', $url, $matches);
-			if(!isset($matches[1])) {
-				preg_match('#http://www.dailymotion.com/embed/video/([A-Za-z0-9]+)#s', $url, $matches);
-				if(strlen($matches[1])){ $media_url = 'dailymotion:_:'.$matches[1]; }
-			}elseif(strlen($matches[1])){
-				$media['id'] = $matches[1];
-				$media['website'] = 'dailymotion';
-			}
-		}elseif(strlen($matches[1])){
-			if(strlen($matches[1])){
-				$media['id'] = $matches[1];
-				$media['website'] = 'dailymotion';
-			}
+		if(preg_match('/dailymotion/', $url)) {
+			return [
+				'id' => strtok(basename($url), '_'),
+				'website' => 'dailymotion'
+			];
 		}
 
 		//YOUTUBE
-		if(preg_match('#(?&lt;=(?:v|i)=)[a-zA-Z0-9-]+(?=&amp;)|(?&lt;=(?:v|i)/)[^&amp;n]+|(?&lt;=embed/)[^"&amp;n]+|(?&lt;=(?:v|i)=)[^&amp;n]+|(?&lt;=youtu.be/)[^&amp;n]+#', $url, $videoid)){
-			if(strlen($videoid[0])) {
-				$media['id'] = $videoid[0];
-				$media['website'] = 'youtube';
-			}
+		if (preg_match('%(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})%i', $url, $match)) {
+			return [
+				'id' => $match[1],
+				'website' => 'youtube'
+			];
 		}
 
-		return $media;
+		return [];
 	}
 }
