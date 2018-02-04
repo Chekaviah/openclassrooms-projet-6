e9c5955598ab4b0680ad94fd26c548eafb4b5a61
Chekaviah <chekaviah@gmail.com>
2018-02-04 13:38:12 +0100

    add comment functionals tests

---
+++ tests/Controller/CommentControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class CommentControllerTest extends WebTestCase
+{
+    public function testCommentAdd()
+    {
+        $client = self::createClient(array(), array(
+            'PHP_AUTH_USER' => 'user',
+            'PHP_AUTH_PW'   => 'user',
+        ));
+        $crawler = $client->request(Request::METHOD_GET, '/trick/view/backside-triple-cork-1440');
+
+        $form = $crawler->selectButton('Laisser un commentaire')->form();
+        $form['comment[content]'] = 'Test de commentaire';
+        $client->submit($form);
+
+        $crawler = $client->followRedirect();
+
+        static::assertSame(1, $crawler->filter('html:contains("Test de commentaire")')->count());
+    }
+}
